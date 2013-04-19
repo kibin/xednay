eec7b0620fe9369b62ee8574e180e309967544eb
Alex Kibin <mr.kibin@gmail.com>
2013-04-20 01:37:40 +0400

    committing

---
+++ lefty.js
@@ -0,0 +1,10 @@
+(function($) {
+	jQuery.fn.leftClick = function(func) {
+		this.on('mousedown', function(e) {
+			if (e.which === 1) {
+				if (func) func();
+				else console.log('left click');
+			} else console.log('middle/right click');
+		});
+	}
+} (jQuery))
